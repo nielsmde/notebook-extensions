29121e60eca0f37e867596f174cc12415871ddf1
Niels Müller <post@nielsm.de>
2015-12-13 19:00:35 +0100

    Revised version of ToC
    Use more jQuery when creating the ToC (e.g don't search for anchor links
    cell-wise) and use full html for the ToC entries. Thereby latex strings
    are visible in the ToC.
    
    Don't hide the div in large windows:
    * If the notebook margin is greater than the width of the ToC-div, do
      not hide
    * Translate in negativ x-direction if the ToC is narrower than the
      notebook margin

--- tableofcontents.js
+++ tableofcontents.js
@@ -1,42 +1,66 @@
-function get_toc_items(cell) {
-    var anchors = cell.element.find('a');
-    var items = [];
-    for (var i = 0;i<anchors.length; i++ ){
-        var a = anchors[i];
-        var h = a.parentElement;
-        if (h.tagName[0] != 'H') continue;
-        var item = document.createElement(h.tagName);
-        var link = document.createElement('a');
-        link.text = h.childNodes[0].textContent;
-        link.href = a.href;
-        item.appendChild(link);
-        items.push(item);
-    }
-    return items;
-}
 
 function toggle_toc() {
     var toc = document.getElementById('toc');
-    if (toc.classList.contains('hidden')) toc.classList.remove('hidden');
-    else toc.classList.add('hidden');
+    if (toc.classList.contains('hidden'))
+        toc.classList.remove('hidden');
+    else 
+        toc.classList.add('hidden');
 }
 
-function table_of_contents() {
-    var cells = Jupyter.notebook.get_cells();
-    var toc = document.getElementById('toc');
-    toc.innerHTML = '';
-    
-    for(var i=0; i<cells.length; i++){
-        var cell = cells[i];
-        if(cell.cell_type == 'markdown'){               
-            var items = get_toc_items(cell);
-            for (var j=0; j<items.length; j++) toc.appendChild(items[j]);
-        }
-    }
+
+function anchor_to_toc_item (index, anchor) {
+    var heading = $(anchor).parent().clone();
+    heading.children().last().remove();
+    heading.removeAttr('id');
+    var item = $(anchor).clone();
+    item.html('')
+    item.removeClass('anchor-link');
+    item.append(heading);
+    return item[0];
+}
+
+function place_toc() {
     var htop = $('#header').height() + 5;
-    var hright = $('#toc').width();
-    toc.setAttribute('style', 'top:'+htop+'px');
+    $('#toc').css('top', htop + 'px');
+}
+
+function translate_toc() {
+    var hright = ($('#notebook').width() - $('#notebook-container').width())/2;
+    var toc = $('#toc');
+    var translate = toc.width() - hright + 35;
+    toc.css('transform', 'translate(' + translate + 'px)')    
+    if (hright < toc.width()) {
+        console.log('if-branch');
+        if (translate > toc.width() - 10)
+            translate = toc.width() - 10;
+
+        toc.hover(
+            function () {toc.css('transform', 'translate(0)')},
+            function () {toc.css('transform', 'translate(' + translate + 'px)')}
+        );
+    } else {
+        toc.unbind('mouseenter');
+        toc.unbind('mouseleave');
+    }
+}
+
+function toc() {
+    var anchors = $('a.anchor-link');
+    var items = anchors.map(anchor_to_toc_item);
+    var toc = $('#toc');
+    toc.children().remove();
+    toc.append(items);
+    
+    place_toc();
     toggle_toc();
+    translate_toc();
+    
+    $(window).resize(function () {
+        place_toc();
+        toc.css('transition', 'all 0s');
+        translate_toc();
+        toc.css('transition', '');
+    });
 }
 
 
@@ -44,4 +68,5 @@ $('head').append($('<link type="text/css" rel="stylesheet" href="/custom/tableof
 
 $('body').append($('<div id="toc" class="hidden" />'));
 
-Jupyter.toolbar.add_buttons_group([{'label':'Table of Contents', 'icon':'fa-list','callback':table_of_contents}])
+Jupyter.toolbar.add_buttons_group([{'label':'Table of Contents', 'icon':'fa-list','callback':toc}])
+
